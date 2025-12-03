ad39b6ee734a3ba42d2073b205c9557b44f25b3a
Natanael Santos <santossilvan347@gmail.com>
2025-12-03 19:48:05 -0300

    atualização

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const scroll=document.querySelector('.js-scroll');
-const windowmetade = window.innerHeight * 0.6;
-function animacao(){
-  
-if(scroll.getBoundingClientRect().top < windowmetade){
-scroll.classList.add('ativo')
+const sections = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const top = section.getBoundingClientRect().top - windowMetade;
+
+    if (top < 0) {
+      section.classList.add('ativo');
+    } else {
+      section.classList.remove('ativo');
+    }
+  });
 }
 
-
-
-
-
-}
-
-window.addEventListener('scroll', animacao)
+window.addEventListener('scroll', animaScroll);
+animaScroll();
