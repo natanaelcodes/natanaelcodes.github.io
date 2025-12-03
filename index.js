1d4d7bb4385ed1f4a1ae4eae945ff4e50c7dcc9e
Natanael Santos <santossilvan347@gmail.com>
2025-12-03 19:43:37 -0300

    melhorias no layout

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-function tipodedado(dado){
+const scroll=document.querySelector('.js-scroll');
+const windowmetade = window.innerHeight * 0.6;
+function animacao(){
+  
+if(scroll.getBoundingClientRect().top < windowmetade){
+scroll.classList.add('ativo')
+}
 
-return typeof dado;
-}
-function nome(){
-  console.log("Natanael");
-}
-addEventListener( "click" , nome);
-var dadospessoais={
-  nome:"natanel",
-  idade:23,
-  altura:1.85,
-}
-var brasilcampeão=[1959,1962, 1970,1994,2002];
-for(var i=0; i< brasilcampeão.length; i++){
-  console.log(`O brasil ganhou a copa de ${i}`);
+
+
+
 
 }
-var possuiCarro=true;
-var possuiCasa=true;
-var darCredito;
-darCredito=(possuiCarro && possuiCasa);
 
+window.addEventListener('scroll', animacao)
